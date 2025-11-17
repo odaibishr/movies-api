16435795c546afe6c8096f0cf34e726c75b85880
Odai Bishr <odaibishr@gmail.com>
2025-11-17 18:33:21 +0300

    create async handler helper for routes

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import * as authController from '../controllers/auth.controller.js';
+import { asyncHandler } from '../utils/helpers.js';
 
 const router = express.Router();
 
-router.post('/register', authController.register);
+router.post('/register', asyncHandler(authController.register));
 
 export default router;
--- utils/helpers.js
+++ utils/helpers.js
@@ -3,3 +3,10 @@ import jwt from 'jsonwebtoken';
 export function generateToken(id) {
     return jwt.sign({ id }, process.env.JWT_SECRET);
 }
+
+
+export function asyncHandler(fn){
+   return (req, res, next) => {
+     return Promise.resolve(fn(req, res, next)).catch(next)
+   }
+}
