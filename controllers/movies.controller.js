686b95171b57af8b930d038fec3ada3a99c6602b
Odai Bishr <odaibishr@gmail.com>
2025-11-18 17:52:08 +0300

    added create movie function

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -1,3 +1,11 @@
+import { Movie } from "../models/index.js";
+
 export async function createMovie(req, res) {
-    
+    const newMovie = await Movie.create({
+        name: req.body.name,
+        genre: req.body.genre,
+        relaseDate: req.body.relaseDate,
+    });
+
+    res.status(201).json(newMovie);
 }
